954b2c0079caaa68c1bee71fc41ced2411219063
Terry Nguyen <terry@terrehbyte.com>
2025-06-09 18:11:15 -0700

    Turn off echo earlier in scripts

--- scripts/Build_Editor_Perforce.bat
+++ scripts/Build_Editor_Perforce.bat
@@ -1,8 +1,8 @@
+@echo off
+
 REM
 REM Place this in the same directory as the project's .uproject file
 REM
-
-@echo off
 
 REM Move to the directory of this script (to allow launchging from P4V)
 cd %~dp0
